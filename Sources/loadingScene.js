14ab0255176e44ee5341a09996aa5d7a857aa448
tovchenko <doctorset@gmail.com>
2014-09-30 17:26:17 +0300

    re-written ObjectBuilder on promises

--- Sources/loadingScene.js
+++ Sources/loadingScene.js
@@ -1,53 +1,33 @@
 /**
- * Created by mc309 on 1/29/14.
+ * Created by tovchenko on 1/29/14.
  */
 
 var es = es || {};
 
 es.LoadingLayer = cc.LayerColor.extend({
-    _finishFn: null,
-    _hasParent: false,
+    _label: null,
 
-    ctor: function() {
+    ctor: function(builderPromise, nextScene) {
         this._super();
-        cc.associateWithNative(this, cc.LayerColor);
+        nextScene && nextScene.retain();
+
         var sz = cc.director.getVisibleSize();
         this.init(cc.color(), sz.width, sz.height);
-
-        this._finishFn = null;
-        this._hasParent = false;
         this._implementContent();
-    },
 
-    step: function(percent) {
-        this._setContentValue(percent);
-
-        if (0 === percent) {
-            this.retain();
-
-            if (this.getParent()) {
-                this.setLocalZOrder(Math.floor(Number.MAX_VALUE));
-                this._hasParent = true;
-            } else {
-                cc.director.runScene(this._getScene());
+        var that = this;
+        builderPromise.then(function() {
+            if (nextScene) {
+                cc.director.popToRootScene();
+                cc.director.pushScene(nextScene);
+                nextScene.release();
             }
-        } else if (100 <= percent) {
-            cc.director.getScheduler().scheduleCallbackForTarget(this, this._finish, 0, 0, 0.5, false);
-        }
-    },
-
-    setFinishHandler: function(handler) {
-        this._finishFn = handler;
-    },
-
-    _finish: function() {
-        if (this._hasParent) {
-            this.removeFromParent();
-        }
-        if (this._finishFn)
-            this._finishFn();
-
-        this.release();
+        }, function() {
+            nextScene.release();
+            throw new Error('Scene loading error!');
+        }, function(percent) {
+            that._setContentValue(percent);
+        });
     },
 
     _implementContent: function() {
@@ -59,46 +39,15 @@ es.LoadingLayer = cc.LayerColor.extend({
 
     _setContentValue: function(val) {
         this._label.setString(val.toFixed(1) + '%');
-    },
-
-    _getScene: function() {
-        var scene = cc.Scene.create();
-        scene.addChild(this);
-        return scene;
     }
 });
 
-es.LoadingLayer.create = function() {
-    return new es.LoadingLayer();
-};
 
-
-
-es.LoadingScene = cc.Class.extend({
-    initWith: function(fileName, loadingLayer, endFn) {
-        var builder = es.ObjectBuilder.create(fileName, loadingLayer.step.bind(loadingLayer));
-        loadingLayer.setFinishHandler(function() {
-            if (!endFn)
-                throw {
-                    name: 'es.LoadingScene Error',
-                    message: 'You must provide callback function.',
-                    toString: function() {return this.name + ": " + this.message}
-                };
-
-            endFn(builder);
-        });
+es.LoadingScene = cc.Scene.extend({
+    ctor: function(builderPromise, nextScene) {
+        this._super();
+        this.init();
+        var layer = new es.LoadingLayer(builderPromise, nextScene);
+        this.addChild(layer);
     }
 });
-
-es.LoadingScene.preload = function(fileName, loadingLayerOrEndFn, endFn) {
-    if (!this._instance)
-        this._instance = new es.LoadingScene();
-
-    var argLen = arguments.length;
-    if(argLen === 2) {
-        this._instance.initWith(fileName, es.LoadingLayer.create(), loadingLayerOrEndFn);
-    } else {
-        this._instance.initWith(fileName, loadingLayerOrEndFn, endFn);
-    }
-    return this._instance;
-};
--- Sources/objectBuilder.js
+++ Sources/objectBuilder.js
@@ -32,20 +32,14 @@ es.ObjectBuilder = cc.Class.extend({
     _fonts: null,
     _fontsCachedPaths: null,
     _jsonData: null,
-    _updateFn: null,
 
     _resIter: null,
     _typeIter: null,
     _totalResCount: null,
     _loadedResCount: null,
+    _deferred: null,
 
-    _updateProgress: function() {
-        ++this._loadedResCount;
-        if (this._updateFn)
-            this._updateFn(100 * this._loadedResCount / this._totalResCount);
-    },
-
-    ctor: function(jsonFileOrFiles, updateFn) {
+    ctor: function(jsonFileOrFiles) {
         this._animations = {};
         this._sounds = {};
         this._music = {};
@@ -55,7 +49,6 @@ es.ObjectBuilder = cc.Class.extend({
         this._data = {};
         this._fonts = [];
         this._jsonData = null;
-        this._updateFn = null;
         this._resIter = 0;
         this._typeIter = es.ResourceType.TEXTURE;
         this._totalResCount = 0;
@@ -68,36 +61,46 @@ es.ObjectBuilder = cc.Class.extend({
 
         this._jsonData = {};
         var that = this;
-        var cnt = 0;
-        for (var i = 0; i < jsonFiles.length; ++i) {
-            cc.loader.loadJson(jsonFiles[i], function(err, json) {
-                if (err) throw err;
 
-                for (var key in json) {
-                    if (json.hasOwnProperty(key)) {
-                        that._jsonData[key] = json[key];
-                    }
-                }
+        var promises = [];
+        jsonFiles.forEach(function(jsonFile) {
+            var deferred = Q.defer();
+            cc.loader.loadJson(jsonFile, deferred.makeNodeResolver());
+            promises.push(deferred.promise);
+        });
 
-                if (++cnt >= jsonFiles.length) {
-                    if (Object.keys(that._jsonData).length === 0)
-                        return;
+        this._deferred = Q.defer();
 
-                    that._updateFn = updateFn;
-                    that._collectInfo(function() {
-                        if (that._updateFn) {
-                            that._updateFn(0);
-                            cc.director.getScheduler().scheduleCallbackForTarget(that, that._loadOneRes, 0, cc.REPEAT_FOREVER, 0, false);
-                        } else {
-                            var loadNext = true;
-                            while (loadNext) {
-                                loadNext = that._loadOneRes();
-                            }
+        Q.all(promises)
+            .then(function(data) {
+                data.forEach(function(dict) {
+                    for (var key in dict) {
+                        if (dict.hasOwnProperty(key)) {
+                            that._jsonData[key] = dict[key];
                         }
+                    }
+                });
+
+                if (0 === Object.keys(that._jsonData).length)
+                    throw new Error('The object builder is empty');
+
+                return that._collectInfo();
+            })
+            .then(function() {
+                var load = function() {
+                    var defer = Q.defer();
+                    cc.director.getScheduler().scheduleCallbackForTarget(that, _.partial(that._loadOneRes, defer.makeNodeResolver()), 0, 0, 0, false);
+                    defer.promise.then(function(exist) {
+                        if (exist) load();
+                        else that._deferred.resolve(that);
                     });
-                }
+                };
+                load();
             });
-        }
+    },
+
+    getLoadingPromise: function() {
+        return this._deferred.promise;
     },
 
     getObjectData: function(objectName) {
@@ -193,13 +196,13 @@ es.ObjectBuilder = cc.Class.extend({
         // music
         var music = object['music'];
         if (music) {
-            var com = res.getComponent(es.AudioCom.identifier);
+            com = res.getComponent(es.AudioCom.identifier);
             if (!com) {
                 com = es.AudioCom.create();
                 res.addComponent(com);
             }
 
-            for (var snd in music) {
+            for (snd in music) {
                 if (music.hasOwnProperty(snd)) {
                     var musName = es.manager.makeResourcePath(es.platform.soundFileName(music[snd]['src']));
                     com.addMusic(snd, musName, music[snd]['loop'], music[snd]['volume']);
@@ -209,7 +212,7 @@ es.ObjectBuilder = cc.Class.extend({
         // sound
         var data = object['data'];
         if (data) {
-            var com = res.getComponent(es.DataCom.identifier);
+            com = res.getComponent(es.DataCom.identifier);
             if (!com) {
                 res.addComponent(es.DataCom.create(data));
             }
@@ -252,8 +255,7 @@ es.ObjectBuilder = cc.Class.extend({
         return res;
     },
 
-    _collectInfo: function(cb) {
-        var that = this;
+    _collectInfo: function() {
         this._animationsCachedPaths = [];
 
         for (var objectName in this._jsonData) {
@@ -312,7 +314,7 @@ es.ObjectBuilder = cc.Class.extend({
                 // music
                 var music = this._jsonData[objectName]['music'];
                 if (music) {
-                    for (var key in music) {
+                    for (key in music) {
                         if (music.hasOwnProperty(key)) {
                             var musicId = music[key]['src'];
                             if (!this._music[musicId]) {
@@ -340,75 +342,80 @@ es.ObjectBuilder = cc.Class.extend({
             }
         }
 
-        cc.loader.load(this._animationsCachedPaths, function() {
-            for (var i = 0; i < that._animationsCachedPaths.length; ++i) {
-                var animator = that._animationsCachedPaths[i];
+        var that = this;
+        var deferred = Q.defer();
+        cc.loader.load(this._animationsCachedPaths, deferred.makeNodeResolver());
+        return deferred.promise
+            .then(function(paths) {
+                paths.forEach(function(animator) {
+                    var dict = cc.loader.getRes(animator);
+                    if (!dict)
+                        throw {
+                            name: 'es.ObjectBuilder Error',
+                            message: 'Animation json file doesn\'t exist or is empty.',
+                            toString: function() {return this.name + ": " + this.message}
+                        };
 
-                var dict = cc.loader.getRes(animator);
-                if (!dict)
-                    throw {
-                        name: 'es.ObjectBuilder Error',
-                        message: 'Animation json file doesn\'t exist or is empty.',
-                        toString: function() {return this.name + ": " + this.message}
-                    };
-
-                var sheets = dict['properties'] && dict['properties']['spritesheets'];
-                if (sheets) {
-                    for (var j = 0; j < sheets.length; ++j) {
-                        var texNameWithoutExt = sheets[j].substr(0, sheets[j].lastIndexOf('.'));
-                        var texName = es.platform.textureFileName(texNameWithoutExt);
-                        sheets[j] = texNameWithoutExt + '.plist';
-                        if (-1 === that._textures.indexOf(texName)) {
-                            that._textures.push(texName);
-                            that._spriteFrames.push(sheets[j]);
-                            that._totalResCount += 2;
+                    var sheets = dict['properties'] && dict['properties']['spritesheets'];
+                    if (sheets) {
+                        for (var j = 0; j < sheets.length; ++j) {
+                            var texNameWithoutExt = sheets[j].substr(0, sheets[j].lastIndexOf('.'));
+                            var texName = es.platform.textureFileName(texNameWithoutExt);
+                            sheets[j] = texNameWithoutExt + '.plist';
+                            if (-1 === that._textures.indexOf(texName)) {
+                                that._textures.push(texName);
+                                that._spriteFrames.push(sheets[j]);
+                                that._totalResCount += 2;
+                            }
                         }
                     }
-                }
-                that._animations[animator] = dict;
-                ++that._totalResCount;
-            }
+                    that._animations[animator] = dict;
+                    ++that._totalResCount;
+                });
 
-            // load other resources
-            that._texturesCachedPaths = that._getFullUrl(that._textures, true);
-            that._spriteFramesCachedPaths = that._getFullUrl(that._spriteFrames, true);
+                // load other resources
+                that._texturesCachedPaths = that._getFullUrl(that._textures, true);
+                that._spriteFramesCachedPaths = that._getFullUrl(that._spriteFrames, true);
 
-            that._soundsCachedPaths = [];
-            for (var key in that._sounds)
-                if (that._sounds.hasOwnProperty(key))
-                    that._soundsCachedPaths.push(es.platform.soundFileName(that._sounds[key]['src']));
-            that._soundsCachedPaths = that._getFullUrl(that._soundsCachedPaths);
+                that._soundsCachedPaths = [];
+                for (var key in that._sounds)
+                    if (that._sounds.hasOwnProperty(key))
+                        that._soundsCachedPaths.push(es.platform.soundFileName(that._sounds[key]['src']));
+                that._soundsCachedPaths = that._getFullUrl(that._soundsCachedPaths);
 
-            that._musicCachedPaths = [];
-            for (var key in that._music)
-                if (that._music.hasOwnProperty(key))
-                    that._musicCachedPaths.push(es.platform.soundFileName(that._music[key]['src']));
-            that._musicCachedPaths = that._getFullUrl(that._musicCachedPaths);
+                that._musicCachedPaths = [];
+                for (key in that._music)
+                    if (that._music.hasOwnProperty(key))
+                        that._musicCachedPaths.push(es.platform.soundFileName(that._music[key]['src']));
+                that._musicCachedPaths = that._getFullUrl(that._musicCachedPaths);
 
-            that._dataCachedPaths = [];
-            for (var key in that._data)
-                if (that._data.hasOwnProperty(key))
-                    that._dataCachedPaths.push(that._data[key]);
+                that._dataCachedPaths = [];
+                for (key in that._data)
+                    if (that._data.hasOwnProperty(key))
+                        that._dataCachedPaths.push(that._data[key]);
 
-            that._fontsCachedPaths = [];
-            for (var key in that._fonts)
-                if (that._fonts.hasOwnProperty(key))
-                    that._fontsCachedPaths.push(es.manager.makeResourcePath(that._fonts[key]));
+                that._fontsCachedPaths = [];
+                for (key in that._fonts)
+                    if (that._fonts.hasOwnProperty(key))
+                        that._fontsCachedPaths.push(es.manager.makeResourcePath(that._fonts[key]));
+            })
+            .then(function() {
+                var list = [].concat(
+                    that._texturesCachedPaths,
+                    that._spriteFramesCachedPaths,
+                    that._armatures,
+                    that._soundsCachedPaths,
+                    that._musicCachedPaths,
+                    that._dataCachedPaths,
+                    that._fontsCachedPaths);
 
-            var list = [].concat(
-                that._texturesCachedPaths,
-                that._spriteFramesCachedPaths,
-                that._armatures,
-                that._soundsCachedPaths,
-                that._musicCachedPaths,
-                that._dataCachedPaths,
-                that._fontsCachedPaths);
-
-            cc.loader.load(list, cb);
-        });
+                var result = Q.defer();
+                cc.loader.load(list, result.makeNodeResolver());
+                return result.promise;
+            });
     },
 
-    _loadOneRes: function() {
+    _loadOneRes: function(cb) {
         var isLast = false;
         var isEmpty = false;
 
@@ -429,7 +436,7 @@ es.ObjectBuilder = cc.Class.extend({
                     isLast = isEmpty = true;
                     break;
                 }
-                var obj = this._spriteFramesCachedPaths[this._resIter];
+                obj = this._spriteFramesCachedPaths[this._resIter];
                 cc.log('Loading sprite frames: ' + obj);
                 cc.spriteFrameCache.addSpriteFrames(obj);
                 isLast = ++this._resIter === this._spriteFramesCachedPaths.length;
@@ -456,7 +463,7 @@ es.ObjectBuilder = cc.Class.extend({
                 }
                 cc.animationCache.addAnimationsWithDictionary(dict);
                 if (anims) {
-                    for (var key in anims) {
+                    for (key in anims) {
                         if (anims.hasOwnProperty(key)) {
                             var animation = cc.animationCache.getAnimation(key);
                             cc.animationCache.addAnimation(animation, keyFileName + key);
@@ -473,7 +480,7 @@ es.ObjectBuilder = cc.Class.extend({
                     break;
                 }
 
-                var obj = this._soundsCachedPaths[this._resIter];
+                obj = this._soundsCachedPaths[this._resIter];
                 cc.log('Loading sound: ' + obj);
                 cc.sys.isNative && cc.audioEngine.preloadEffect(obj);
                 isLast = ++this._resIter === this._soundsCachedPaths.length;
@@ -485,7 +492,7 @@ es.ObjectBuilder = cc.Class.extend({
                     break;
                 }
 
-                var obj = this._musicCachedPaths[this._resIter];
+                obj = this._musicCachedPaths[this._resIter];
                 cc.log('Loading music: ' + obj);
                 // cc.sys.isNative && cc.audioEngine.preloadMusic(obj);
                 isLast = ++this._resIter === this._musicCachedPaths.length;
@@ -497,27 +504,25 @@ es.ObjectBuilder = cc.Class.extend({
                     break;
                 }
 
-                var obj = this._armatures[this._resIter];
+                obj = this._armatures[this._resIter];
                 cc.log('Loading armature: ' + obj);
                 ccs.armatureDataManager.addArmatureFileInfo(obj);
                 isLast = ++this._resIter === this._armatures.length;
                 break;
         }
 
-        if (!isEmpty)
-            this._updateProgress();
+        if (!isEmpty) {
+            this._deferred.notify(100 * ++this._loadedResCount / this._totalResCount);
+        }
 
         if (isLast) {
             this._resIter = 0;
             if (++this._typeIter === es.ResourceType.END) {
-                if (this._updateFn) {
-                    cc.director.getScheduler().unscheduleCallbackForTarget(this, arguments.callee);
-                    if (0 === this._loadedResCount)
-                        this._updateFn(100);
-                }
+                cb && cb(null, false);
                 return false;
             }
         }
+        cb && cb(null, true);
         return true;
     },
 
@@ -563,6 +568,7 @@ es.ObjectBuilder = cc.Class.extend({
     }
 });
 
-es.ObjectBuilder.create = function(jsonFileOrFiles, finishFn) {
-    return new es.ObjectBuilder(jsonFileOrFiles, finishFn);
+es.ObjectBuilder.create = function(jsonFileOrFiles) {
+    var builder = new es.ObjectBuilder(jsonFileOrFiles);
+    return builder.getLoadingPromise();
 };
